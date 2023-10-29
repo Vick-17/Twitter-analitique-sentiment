ae8b54b89747f51106852fb5604ccc52f87256a8
Vick-17 <vega.victor.0712@gmail.com>
2023-10-29 16:36:12 +0100

    init commit / fin

---
+++ index.js
@@ -0,0 +1,52 @@
+const Twitter = require('twitter-lite');
+const language = require("@google-cloud/language");
+const languageClient = new language.LanguageServiceClient();
+
+const user = new Twitter({
+  consumer_key: "TWITTER_KEY",
+  consumer_secret: "TWITTER_KEY",
+});
+
+// Enveloppe le code suivant dans une fonction asynchrone qui est appelée
+// immédiatement afin que nous puissions utiliser les instructions "await".
+(async function() {
+    try {
+      // retrouver le bearer token de twitter
+      let response = await user.getBearerToken();
+      // Construire notre client API avec le jeton du porteur.
+      const app = new Twitter({
+        bearer_token: response.access_token,
+      });
+
+      // Recherche de tweets récents à partir de l'API Twitter
+      response = await app.get(`/search/tweets`, {
+        q: "Lionel Messi",
+        lang: "fr",
+        count: 100,
+      });
+
+      let allTweets = "";
+      for (tweet of response.statuses) {
+        allTweets += tweet.text + "\n";
+      }
+
+      const sentimentScore = await getSentimentScore(allTweets);
+      console.log(`The sentiment about ${query} is: ${sentimentScore}`);
+
+    } catch(e) {
+        console.log("Une erreur s'est produite lors de l'appel de l'API Twitter.");
+        console.dir(e);
+    }
+})();
+
+async function getSentimentScore(text) {
+    const document = {
+        content: text,
+        type: 'PLAIN_TEXT',
+    };
+
+    const [result] = await languageClient.analyzeSentiment({document: document});
+    const sentiment = result.documentSentiment;
+
+    return sentiment.score
+}
